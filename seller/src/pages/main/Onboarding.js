17812874986e5ae82e0b422e26bf1c89699f4e6f
HarshalHDave <71221177+HarshalHDave@users.noreply.github.com>
2022-08-20 12:04:43 +0530

    fix: onboarding issues

---
+++ seller/src/pages/main/Onboarding.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { Button, Typography, Container, Card, CardContent, Stack, Pagination } from '@mui/material';
+import { PersonalDetailsForm, AddressDetailsForm, ArtistDetailsForm } from 'src/components';
+
+export default function Onboarding() {
+    const [page, setPage] = useState(1);
+
+    const handleChange = (formValues) => {
+        console.log(formValues);
+    };
+
+    const pages = [
+        <PersonalDetailsForm onFormValuesChange={handleChange} />,
+        <AddressDetailsForm />,
+        <ArtistDetailsForm />
+    ];
+
+    return (
+        <Container
+            maxWidth="md"
+            sx={{
+                mt: 10,
+                backgroundColor: '#F8F8FF'
+            }}
+        >
+            <Card>
+                <CardContent>
+                    <Typography variant="h3">Welcome to 3Commerce</Typography>
+                    <Typography variant="body1" mt={1}>
+                        Let's set up your profile
+                    </Typography>
+                    <Stack alignItems="center" gap={4} mt={3}>
+                        {pages[page - 1]}
+                        <Pagination count={3} onChange={(e, val) => setPage(val)} hidePrevButton />
+                        {val === 3 && (
+                            <Button variant="contained" color="primary">
+                                {' '}
+                                Submit{' '}
+                            </Button>
+                        )}
+                    </Stack>
+                </CardContent>
+            </Card>
+        </Container>
+    );
+}
